4a999c11dea8f286f25c5dc20234851d10d3d0be
Kaylee Cummings <kayleecummings@Kaylees-MacBook-Pro.local>
2016-03-15 15:45:05 -0500

    setup routing

---
+++ app/controllers/SearchCtrl.js
@@ -0,0 +1,15 @@
+"use strict";
+
+app.controller("SearchCtrl", [
+  "$scope",
+
+  function($scope) {
+    $scope.searchUserMovie = "";
+
+    // this function is triggered by search button
+    $scope.search = function() {
+    // takes searchUserMovie string and shows matching movie posters
+    }
+  }
+
+]);
