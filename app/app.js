91b9ea30abc08e7fb214979cfbbaf0fc2a334ca8
Thomas Buida <thomasbuida@gmail.com>
2016-03-15 19:36:53 -0500

    Added basic user authentication/login/logout/registering

--- app/app.js
+++ app/app.js
@@ -1,18 +1,33 @@
 "use strict";
 
-let app = angular.module("MovieApp", ["ngRoute"]);
+let app = angular.module("MovieApp", ["ngRoute", "firebase"]).constant('firebaseURL', "https://nss-team-america.firebaseio.com");
+
+
+/*
+  Define a promise for any view that needs an authenticated user
+  before it will resolve (see below)
+ */
+let isAuth = (authFactory) => new Promise((resolve, reject) => {
+  if (authFactory.isAuthenticated()) {
+    console.log("User is authenticated, resolve route promise");
+    resolve();
+  } else {
+    console.log("User is not authenticated, reject route promise");
+    reject();
+  }
+});
 
 app.config(["$routeProvider",
   function ($routeProvider) {
     $routeProvider.
       when("/", {
-        templateUrl: "partials/how-to.html"
-        // resolve: { isAuth }
+        templateUrl: "partials/how-to.html",
+        resolve: { isAuth }
       }).
       when("/find", {
         templateUrl: "partials/find-new.html",
-        controller: "FindCtrl"
-        // resolve: { isAuth }
+        controller: "FindCtrl",
+        resolve: { isAuth }
       }).
       when("/login", {
         templateUrl: "partials/login.html",
@@ -24,15 +39,36 @@ app.config(["$routeProvider",
       }).
       when("/search", {
         templateUrl: "partials/filter.html",
-        controller: "SearchCtrl"
-        // resolve: { isAuth }
+        controller: "SearchCtrl",
+        resolve: { isAuth }
       }).
       when("/user-movies", {
         templateUrl: "partials/user-movies.html",
-        controller: "UserMovieCtrl"
-        // resolve: { isAuth }
+        controller: "UserMovieCtrl",
+        resolve: { isAuth }
       }).
       otherwise({
         redirectTo: "/"
       });
   }]);
+
+
+/*
+  When the application first loads, redirect the user to the login
+  form if there is no authentication
+ */
+app.run([
+  "$location",
+
+  ($location) => {
+    let appRef = new Firebase("https://nss-team-america.firebaseio.com");
+
+    appRef.onAuth(authData => {
+      if (!authData) {
+
+        console.log("onAuth detected unauthenticated client");
+        $location.path("/login");
+      }
+    });
+  }
+]);
