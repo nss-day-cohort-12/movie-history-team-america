c81a4ab731e694d00e9b697b48942f35cb2e58e2
Thomas Buida <thomasbuida@gmail.com>
2016-03-16 11:01:37 -0500

    added test json file

--- app/controllers/FindCtrl.js
+++ app/controllers/FindCtrl.js
@@ -8,20 +8,27 @@ app.controller("FindCtrl", [
     $scope.findTitle = "";
     $scope.movie = false;
     $scope.movieImdbID = "";
-    $scope.posterSrc = "http://img.omdbapi.com/?i=tt0113277&apikey=7c212437";
 
     // this function is triggered by find button
     $scope.find = function() {
     // takes findTitle string and returns search results from OMDB API
+      MovieFactory($scope.findTitle).then(
+      	movieCollection => {console.log(movieCollection)
+      		$scope.movie = movieCollection;
+          $scope.movieImdbId = $scope.movie.imdbID;
+      	},
+      	err => console.log(err)
+      	);
+    }
 
-    MovieFactory($scope.findTitle).then(
-    	movieCollection => {console.log(movieCollection)
-    		$scope.movie = movieCollection;
-        $scope.posterSrc = `http://img.omdbapi.com/?i=${$scope.movie.imdbID}&apikey=7c212437`;
-        $scope.movieImdbId = $scope.movie.imdbID;
-    	},
-    	err => console.log(err)
-    	);
+
+    $scope.add = function () {
+      $scope.movie = false;
+      $scope.findTitle = "";
+
+      //take movie and add tracked id of true
+      
+
     }
   }
 
