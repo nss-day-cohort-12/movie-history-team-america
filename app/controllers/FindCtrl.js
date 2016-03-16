0416d4f8d6b960c5c0df23a4c3aa08cc46cc7ba6
Thomas Buida <thomasbuida@gmail.com>
2016-03-16 12:41:09 -0500

    Movie added to firebase

--- app/controllers/FindCtrl.js
+++ app/controllers/FindCtrl.js
@@ -3,8 +3,9 @@
 app.controller("FindCtrl", [
   "$scope",
   "MovieFactory",
+  "$http",
 
-  function($scope, MovieFactory) {
+  function($scope, MovieFactory, $http) {
     $scope.findTitle = "";
     $scope.movie = false;
     $scope.movieImdbID = "";
@@ -23,13 +24,38 @@ app.controller("FindCtrl", [
 
 
     $scope.add = function () {
+
+      //take movie and add tracked id of true
+
+      // POST the song to Firebase
+      $http.post(
+        "https://nss-team-america.firebaseio.com/movies.json",
+
+        // Remember to stringify objects/arrays before
+        // sending them to an API
+        JSON.stringify({
+          title: $scope.movie.Title,
+          year: $scope.movie.Year,
+          actors: $scope.movie.Actors,
+          rating: "0",
+          watched: false,
+          tracked: true,
+          imdbID: $scope.movie.imdbID
+        })
+
+      // The $http.post() method returns a promise, so you can use then()
+      ).then(
+        () => console.log("Added movie to firebase"),// Handle resolve
+        (response) => console.log(response)  // Handle reject
+      );
+
+
       $scope.movie = false;
       $scope.findTitle = "";
 
-      //take movie and add tracked id of true
-      
+    };
 
     }
-  }
+
 
 ]);
